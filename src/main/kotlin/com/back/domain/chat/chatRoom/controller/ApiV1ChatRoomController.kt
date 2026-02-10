85ccd09b1f5310cd2d2608ba4b62b0590bf7369f
jhs512 <jangka512@gmail.com>
2026-02-10 11:20:20 +0900

    0012

--- src/main/kotlin/com/back/domain/chat/chatRoom/controller/ApiV1ChatRoomController.kt
+++ src/main/kotlin/com/back/domain/chat/chatRoom/controller/ApiV1ChatRoomController.kt
@@ -1,12 +1,35 @@
 package com.back.domain.chat.chatRoom.controller
 
+import com.back.domain.chat.chatRoom.entity.ChatRoom
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/v1/chat/rooms")
 class ApiV1ChatRoomController {
+    val chatRooms: MutableList<ChatRoom> = mutableListOf(
+        ChatRoom(
+            1,
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            "풋살하실 분?"
+        ),
+        ChatRoom(
+            2,
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            "농구 하실 분?"
+        ),
+        ChatRoom(
+            3,
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            "야구 하실 분?"
+        )
+    )
+
     @GetMapping
-    fun getItems() = listOf("room1", "room2", "room3")
+    fun getItems() = chatRooms
 }
