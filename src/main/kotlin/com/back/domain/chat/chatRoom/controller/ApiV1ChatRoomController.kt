7f3bacd6a6f7c01ac268a4e56633e721e2a99a0a
jhs512 <jangka512@gmail.com>
2026-02-10 11:12:11 +0900

    0010

---
+++ src/main/kotlin/com/back/domain/chat/chatRoom/controller/ApiV1ChatRoomController.kt
@@ -0,0 +1,12 @@
+package com.back.domain.chat.chatRoom.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/chat/rooms")
+class ApiV1ChatRoomController {
+    @GetMapping
+    fun getItems() = "채팅방 목록!"
+}
